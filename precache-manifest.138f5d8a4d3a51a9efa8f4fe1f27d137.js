21ae8a2ea41ae02ba33c53585e8797f3ae6ad3b6
Said Mounaim <saidmounaim00@gmail.com>
2020-12-27 19:13:50 +0100

    Updates

---
+++ precache-manifest.138f5d8a4d3a51a9efa8f4fe1f27d137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea65c57bf6b6e8f1ac4f209cd09e46b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc57e2d309326042644e",
+    "url": "./static/css/main.384f8a40.chunk.css"
+  },
+  {
+    "revision": "f91cef80e21c5d1fdcb3",
+    "url": "./static/js/2.f5bd3cc5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f5bd3cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc57e2d309326042644e",
+    "url": "./static/js/main.086e3738.chunk.js"
+  },
+  {
+    "revision": "e3da5306e92a63ea0324",
+    "url": "./static/js/runtime-main.7afd4ceb.js"
+  }
+]);
